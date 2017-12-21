c14f6b4ce0d1271d59b027b2d023932b631ca1d7
jtolp <jtolp@DESKTOP-UOVU2I4>
2017-12-21 03:05:21 +0200

    Solution commit

---
+++ ArvatoTestAssignment/PageObjects.cs
@@ -0,0 +1,22 @@
+﻿using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+
+
+namespace ArvatoTestAssignment
+{
+    class PageObjects
+    {
+        [FindsBy(How = How.Id, Using = "menu-item-57")]
+        public IWebElement ITDevelopmentCentreBtn { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "/html/body/div[2]/div[2]/div/a/button")]
+        public IWebElement OpenPositionsBtn { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "/html/body/div[2]/div[4]/div[1]/div[2]/div[1]/h3[2]")]
+        public IWebElement WorkAtArvatoText { get; set; }
+
+        [FindsBy(How = How.XPath, Using = "/html/body/div[2]/div[4]/div[2]/div[1]/h4")]
+        public IWebElement WeOfferYouText { get; set; }
+
+    }
+}
---
+++ ArvatoTestAssignment/UIAutomationTests.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Threading;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Support.PageObjects;
+using OpenQA.Selenium.Chrome;
+using NUnit.Framework;
+
+namespace ArvatoTestAssignment
+{
+    [TestFixture]
+    public class UIAutomationTests
+    {
+        static IWebDriver driver = new ChromeDriver();
+
+        [SetUp]
+        public void Invoke()
+        {
+            driver.Navigate().GoToUrl("http://arvato.ee/en/frontpage/");
+            driver.Manage().Window.Maximize();
+            Assert.AreEqual("Arvato Services Estonia", driver.Title);
+        }
+        [Test]
+        public void UIAutomationTest1()
+        {
+            PageObjects PageObjects = new PageObjects();
+            PageFactory.InitElements(driver, PageObjects);
+
+            PageObjects.ITDevelopmentCentreBtn.Click();
+            Thread.Sleep(1000);
+            PageObjects.OpenPositionsBtn.Click();
+            Thread.Sleep(1000);
+
+            if (PageObjects.WorkAtArvatoText.Displayed)
+            {
+                Console.WriteLine(PageObjects.WorkAtArvatoText.Text.ToString());
+            }
+            else
+            {
+                Assert.Fail();
+            }
+
+
+        }
+        [Test]
+        public void UIAutomationTest2()
+        {
+            //Future UIAutomationTest2
+        }
+        [Test]
+        public void UIAutomationTest3()
+        {
+            //Future UIAutomationTest3
+        }
+        [TearDown]
+        public void Dismiss()
+        {
+            driver.Quit();
+        }
+    }
+}
